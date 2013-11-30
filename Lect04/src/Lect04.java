7b2c2c3a3fb53b5c790ce65eecc9b241c61642c6
wojtek <wojtek@wojtek>
2013-11-30 11:48:55 +0100

    addind simple thread example

---
+++ Lect04/src/Lect04.java
@@ -0,0 +1,42 @@
+
+public class Lect04 {
+
+	/**
+	 * @param args
+	 */
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//MyTask task1 = new MyTask(); 
+		
+		Thread test1 = new Thread(new Runnable() {
+			public void run() {
+				for(;;){
+		    	System.out.println("jak sie masz");
+		    	
+		    	try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					break;
+					
+				}
+		    	
+				}
+				
+			}
+		});
+		
+		test1.start();
+		
+		try {
+			Thread.sleep(10000);
+			test1.interrupt();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
---
+++ Lect04/src/MyTask.java
@@ -0,0 +1,12 @@
+public class MyTask implements Runnable {
+
+	/**
+	 * @param args
+	 */
+
+	@Override
+	public void run() {
+		System.out.println("Kaszanak Kaszanak");
+
+	}
+}
